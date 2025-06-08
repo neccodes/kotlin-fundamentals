005ff0c457f07784ca96e49dd8852473ced221f8
neccodes <neccabrel22@gmail.com>
2025-06-08 17:53:57 +0400

    first commit

---
+++ src/dataTypes/dataTypes.kt
@@ -0,0 +1,45 @@
+package dataTypes
+
+fun main(args: Array<String>) {
+
+    println("Hello KotlinVille!")
+    var userName: String = "Hatem"
+    userName = "Karim"
+    var age: Int = 20
+    age = 23
+    println("Hello, my name is ${userName.uppercase()} and i am $age years old.")
+    println("Hello ${userName.uppercase()}, your age is $age!")
+
+    val integerMinValue = Int.MIN_VALUE
+    val integerMaxvalue = Int.MAX_VALUE
+    println("Integer max Value = $integerMaxvalue")
+    println("integer min Value = $integerMinValue")
+
+    val byteMaxvalue: Byte = Byte.MAX_VALUE
+    val byteMinValue: Byte = Byte.MIN_VALUE
+    println("byte max value = $byteMaxvalue")
+    println("byte min value = $byteMinValue")
+
+    val shortMaxValue: Short = Short.MAX_VALUE
+    val  shortMinValue: Short = Short.MIN_VALUE
+    println("short min value = $shortMinValue")
+    println("short max value = $shortMaxValue")
+
+    val longMaxValue = Long.MAX_VALUE
+    val longMinValue = Long.MIN_VALUE
+    println("long max value = $longMaxValue")
+    println("long min value = $longMinValue")
+
+    val myNumber = 28L
+
+    // Float and Double
+    val firstNumber = 2.5F
+    val secondNumber = 2.5
+    println("my number is $firstNumber and my second number is $secondNumber.")
+
+    // Char and Boolean
+    val myCharValue: Char = 'C'
+    val myBooleanValue: Boolean = true
+    println("my Char value = $myCharValue")
+    println("my boolean value = $myBooleanValue")
+}
